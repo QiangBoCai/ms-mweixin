13c9d61c4ab8c8d8715ed2e768762f96ee85adb6
killfen <killfen@126.com>
2017-03-29 14:05:44 +0800

    项目初始化，

--- src/main/java/com/mingsoft/mwebsite/biz/IWebsiteLogBiz.java
+++
@@ -1,6 +0,0 @@
-package com.mingsoft.mwebsite.biz;
-
-import com.mingsoft.base.biz.IBaseBiz;
-
-public interface IWebsiteLogBiz extends IBaseBiz {
-}
--- src/main/java/com/mingsoft/mwebsite/dao/IWebsiteLogDao.java
+++
@@ -1,9 +0,0 @@
-package com.mingsoft.mwebsite.dao;
-
-import com.mingsoft.base.dao.IBaseDao;
-import com.mingsoft.base.entity.BaseEntity;
-import com.mingsoft.basic.entity.RoleEntity;
-
-public interface IWebsiteLogDao extends IBaseDao{
-	
-}
